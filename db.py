b5c447804113b99c74e56e3ba2020ba67c2f5b18
Sean O'Connor <soc1641@icloud.com>
2025-11-30 00:25:08 -0600

    moved db.py to new file to handle db connections like that

---
+++ db.py
@@ -0,0 +1,17 @@
+# --- Database Configuration ---
+db_config = {
+    'host': 'localhost',
+    'database': 'library',
+    'user': 'root',      # <-- IMPORTANT: Change to your MySQL username
+    'password': 'maggie1641' # <-- IMPORTANT: Change to your MySQL password
+}
+
+# --- Helper Function to get DB connection ---
+def get_db_connection():
+    """Establishes a connection to the database."""
+    try:
+        conn = mysql.connector.connect(**db_config)
+        return conn
+    except Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        return None
