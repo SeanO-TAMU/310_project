9fac67f3197ee6aabc82b9effa2a0ec62877da31
Sean O'Connor <soc1641@icloud.com>
2025-11-30 00:46:13 -0600

    Added example auth check in books routes for the create book

--- routes/books.py
+++ routes/books.py
@@ -1,11 +1,27 @@
 from flask import Blueprint, request, jsonify
 from db import get_db_connection
 
+from auth import require_token 
+
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 @books_bp.get("/")
 def get_all_books():
-    pass
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({'error': 'Database connection failed'}), 500
+    
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Books")
+    books = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    if books == []:
+        return jsonify({'error': 'No books in database'}), 404
+    
+    return jsonify({'books': books})
 
 @books_bp.get("/<int:id>")
 def get_book(id):
@@ -24,10 +40,28 @@ def get_book(id):
     
     return jsonify({'book': book})
 
+
+
+# need auth token on the below routes, also need to check for role
 @books_bp.post("/")
 def create_book():
-    pass
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({'error': 'Database connection failed'}), 500
+    
+    cursor = conn.cursor(dictionary=True)
 
 @books_bp.put("/<int:id>")
 def update_book():
-    pass
+    conn = get_db_connection()
+    if conn is None:
+        return jsonify({'error': 'Database connection failed'}), 500
+    
+    cursor = conn.cursor(dictionary=True)
+
+    user, error = require_token()
+    if error:
+        return error
+
+    if user["role"] != "manager":
+        return {"error": "Forbidden"}, 403
