8da7e42175004a0142e6cd3190d51ada90ca9241
Sean O'Connor <soc1641@icloud.com>
2025-12-01 13:34:29 -0600

    added appropriate permissions to orderItems routes

--- routes/orderItems.py
+++ routes/orderItems.py
@@ -9,9 +9,16 @@ orderitems_bp = Blueprint("orderitems", __name__, url_prefix="/orderitems")
 
 @orderitems_bp.get("/")
 def get_all_orderitems():
+    user, error = require_token()
+    if error:
+        return error
+    
     conn = get_db_connection()
     if conn is None:
         return jsonify({'error': 'Database connection failed'}), 500
+    
+    if user["role"] != "manager":
+        return {"error": "Forbidden"}, 403
     
     cursor = conn.cursor(dictionary=True)
 
@@ -28,6 +35,10 @@ def get_all_orderitems():
 
 @orderitems_bp.get("/<int:id>")
 def get_orderitem(id):
+    user, error = require_token()
+    if error:
+        return error
+    
     conn = get_db_connection()
     if conn is None:
         return jsonify({'error': 'Database connection failed'}), 500
@@ -36,16 +47,29 @@ def get_orderitem(id):
 
     cursor.execute("SELECT orderitemID, bookID, billingID, price, order_type FROM OrderItems WHERE orderitemID = %s", (id,))
     orderitem = cursor.fetchone()
-    cursor.close()
-    conn.close()
 
     if orderitem is None:
         return jsonify({'error': 'OrderItem not found in database'}), 404
+
+    #check to make sure user is either a manager or the userid matches the one that is in the borderItems billing
+    cursor.execute("SELECT userID from Billings WHERE billingID = %s", (orderitem["billingID"],))
+    billing = cursor.fetchone()
+
+    if user["role"] != "manager" and billing["userID"] != user["userID"]:
+        return {"error": "Forbidden"}, 403
+
+    cursor.close()
+    conn.close()
+
     
     return jsonify({'orderitem': orderitem})
 
 @orderitems_bp.get("/billing/<int:id>")
-def get_orderitem_by_billing(id):
+def get_orderitems_by_billing(id):
+    user, error = require_token()
+    if error:
+        return error
+    
     conn = get_db_connection()
     if conn is None:
         return jsonify({'error': 'Database connection failed'}), 500
@@ -54,11 +78,18 @@ def get_orderitem_by_billing(id):
 
     cursor.execute("SELECT orderitemID, bookID, billingID, price, order_type FROM OrderItems WHERE billingID = %s", (id,))
     orderitems = cursor.fetchall()
-    cursor.close()
-    conn.close()
 
     if orderitems == []:
         return jsonify({'error': 'OrderItems not found in database'}), 404
+
+    cursor.execute("SELECT userID from Billings WHERE billingID = %s", (id,))
+    billingID = cursor.fetchone()
+
+    if user["role"] != "manager" and billingID["userID"] != user["userID"]:
+        return {"error": "Forbidden"}, 403
+
+    cursor.close()
+    conn.close()
     
     return jsonify({'orderitems': orderitems})
 
@@ -70,15 +101,27 @@ def create_orderitem():
     price = float(data.get("price"))
     order_type = data.get("order_type")
 
+    user, error = require_token()
+    if error:
+        return error
+
     conn = get_db_connection()
     if conn is None:
         return jsonify({'error': 'Database connection failed'}), 500
     
     cursor = conn.cursor(dictionary=True)
     
-    user, error = require_token()
-    if error:
-        return error
+    if user["role"] == "manager":
+        return {"error": "Forbidden"}, 403
+    
+    cursor.execute("SELECT userID FROM Billings WHERE billingID = %s", (billingID,))
+    billing = cursor.fetchone()
+
+    if billing is None:
+        return {"error": "Billing not found"}, 404
+    
+    if billing["userID"] != user["userID"]:
+        return {"error": "Forbidden"}, 403
     
     cursor.execute("INSERT INTO OrderItems (bookID, billingID, price, order_type) VALUES (%s, %s, %s, %s)", (bookID, billingID, price, order_type))
     
