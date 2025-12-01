46081c8cbb7bf71d0b8dbc6960f5afb356e3e3d9
Sean O'Connor <soc1641@icloud.com>
2025-11-30 23:38:23 -0600

    added blueprint routes

--- server.py
+++ server.py
@@ -3,9 +3,12 @@ import mysql.connector
 from mysql.connector import Error
 
 from routes.users import users_bp
+from routes.books import books_bp
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(users_bp)
 app.register_blueprint(users_bp)
 
 if __name__ == '__main__':
